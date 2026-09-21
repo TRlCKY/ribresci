ee740825bd4d0faf11797e999a7caaab8192f434
Riccardo Bresci <ribresci@c1r6p10.42firenze.it>
2026-09-21 17:50:28 +0200

    OKE

--- Exam03/Level_5/py_string_sculptor.py
+++ Exam03/Level_5/py_string_sculptor.py
@@ -1,23 +1,26 @@
 def string_sculptor(text: str) -> str:
     x = len(text)
     n = 0
+    new_text = ""
     if x == 0:
         return text
 
     i = 0
     while i < x:
-        while i < x and text[i] <= "z" and text[i] >= "a" and text[i] <= "Z" and text[i] >= "A":
+        while (i < x and text[i] <= "a" and text[i] >= "z"
+               and text[i] <= "A" and text[i] >= "Z"):
+            new_text += text[i]
             i = i + 1
         n = 0
-        while i < x and text[i] >= "a" and text[i] <= "z" or text[i] >= "A" and text[i] <= "Z":
+        while (i < x and text[i] >= "a" and text[i] <= "z" or text[i] >= "A"
+               and text[i] <= "Z"):
             if n % 2 == 0 and text[i] >= "A" and text[i] <= "Z":
-                # Conversione da maiuscola a minuscola
-                print()
+                new_text += text[i].lower()
             elif n % 2 == 1 and text[i] >= "a" and text[i] <= "z":
-                text[i] = text[i].capitalize()
+                new_text += text[i].upper()
             n = n + 1
             i = i + 1
-    return text
+    return new_text
 
 
 def main():
